8beef528c225b60516be21916d4266a35f4d3c74
youssef-salama <youssifsalama47@gmail.com>
2026-03-15 05:53:51 +0200

    refactor: improve naming conventions

--- HistoryService/Data/HistoryDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class HistoryDbContext : DbContext
-{
-    public HistoryDbContext(DbContextOptions<HistoryDbContext> options)
-        : base(options) { }
-
-    public DbSet<CalculationResult> CalculationResults => Set<CalculationResult>();
-}
--- HistoryService/Models/CalculationResult.cs
+++
@@ -1,6 +0,0 @@
-public class CalculationResult
-{
-    public int Id { get; set; }
-    public double Result { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
